fd27e9a9594af8e42df3aaf1d2129dd22ff959f5
RealJohnHarold <21-01210@g.batstate-u.edu.ph>
2024-04-26 15:02:42 +0800

    Version 1 SideBar Navigation - Dashboard

--- app/dashboard/visitor-history/page.tsx
+++ app/dashboard/visitor-history/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import SideBar from '@/app/_components/dashboard-page/SideBar';
 
-export default function GraphsCharts() {
+export default function VisitorHistory() {
     return(
         <>
         <SideBar />
